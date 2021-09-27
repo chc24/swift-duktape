fa3ba35df7c88232fb482592d4fe275f0f4201e0
Calvin Chueh <chchueh24@gmail.com>
2021-09-27 07:13:05 -0700

    fix

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
             path: "Sources/CDuktape",
             exclude: ["swift-duktape"],
             cSettings: [
-                .headerSearchPath("include/")
+                .headerSearchPath("include/CDuktape.h")
             ]
         ),
         .target(
