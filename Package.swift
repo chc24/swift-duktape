26fb009b26175b189d7e86b2082c006ac1cfb649
Calvin Chueh <chchueh24@gmail.com>
2021-09-27 07:31:35 -0700

    fix absolute path

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
             ]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -27,7 +23,7 @@ let package = Package(
             path: "Sources/CDuktape",
             exclude: ["swift-duktape"],
             cSettings: [
-                .headerSearchPath("include/CDuktape.h")
+                .headerSearchPath("include/")
             ]
         ),
         .target(
