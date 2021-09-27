cc6dcb45561c6409a3f23f2518b0d767a8efd680
Calvin Chueh <chchueh24@gmail.com>
2021-09-27 07:05:17 -0700

    add cduktape

--- Package.swift
+++ Package.swift
@@ -9,7 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "swift-duktape",
-            targets: ["swift-duktape"]),
+            targets: [
+                "swift-duktape",
+                "CDuktape"
+            ]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
